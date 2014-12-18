1daeedde2cd8597e047b6a6d7fc737f103fa4ac8
Walter Moreira <walter@waltermoreira.net>
2014-12-18 11:52:26 -0600

    Add example of supervisor event and member join

--- example/handler/my_handler.py
+++ example/handler/my_handler.py
@@ -1,5 +1,5 @@
 from base_handler import BaseHandler
-from utils import truncated_stdout, with_payload
+from utils import truncated_stdout, with_payload, with_member_info
 
 
 class MyHandler(BaseHandler):
@@ -7,4 +7,23 @@ class MyHandler(BaseHandler):
     @truncated_stdout
     @with_payload
     def hello(self, who=None):
+        """A custom user event."""
+
         print("Hello there, {}!".format(who))
+
+    @with_payload
+    def supervisor(self, **kwargs):
+        """This event gets fired for change of state in supervisor."""
+
+        print("Got a supervisor event with payload:")
+        print(kwargs)
+
+    @with_member_info
+    def member_join(self, members):
+        """This event gets fired on members joining.
+
+        ``members`` is the dict with new members'
+
+        """
+        print("Hello to new members:")
+        print(members)

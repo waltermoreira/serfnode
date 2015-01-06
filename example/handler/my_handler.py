8265b52556ad048ee66ab96362aca6d7df566640
Walter Moreira <walter@waltermoreira.net>
2015-01-06 14:24:10 -0600

    Add example of using new serfnode

---
+++ example/handler/my_handler.py
@@ -0,0 +1,12 @@
+from base_handler import BaseHandler
+import supervisor
+
+
+class MyHandler(BaseHandler):
+
+    def setup(self):
+        super(MyHandler, self).setup()
+        supervisor.start_docker(
+            'rabbitmq.conf',
+            'rabbitmq',
+            '--rm {} dockerfile/rabbitmq'.format(self.volumes))

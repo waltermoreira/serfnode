e0e643922500de47c25d633a044dccbed102e088
Walter Moreira <walter@waltermoreira.net>
2015-01-13 12:09:37 -0600

    Add update to serfnode_init

--- builder/entry.sh
+++ builder/entry.sh
@@ -3,8 +3,18 @@
 cat /proc/mounts | awk '{print $2}' | grep '/target' > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Please, mount the destination directory in '/target'"
-    echo 'For example: docker run -it -v $(pwd):/target serfnode_init'
+    echo 'For example: docker run -it -v $(pwd):/target serfnode_init init'
     exit 1
 fi
 
-cookiecutter /template
+if [ "$1" == "init" ]; then
+    cookiecutter /template
+else
+    # just update roles
+    (
+        cd /tmp;
+        cookiecutter --no-input /roles_template;
+    )
+    rm -rf deploy/roles
+    cp -r /tmp/my_serfnode/deploy/roles deploy/
+fi

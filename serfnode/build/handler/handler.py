4c04979de66cf5d0858ff00002ef40df196ccd05
Walter Moreira <walter@waltermoreira.net>
2015-01-16 16:45:02 -0600

    Remove prints that interfere with json output

--- serfnode/build/handler/handler.py
+++ serfnode/build/handler/handler.py
@@ -6,8 +6,6 @@ from base_handler import BaseHandler
 try:
     from my_handler import MyHandler
 except ImportError:
-    print "Could not import user's handler."
-    print "Defaulting to dummy handler."
     MyHandler = BaseHandler
 
 

4eb15d1b6e69ea3d988b7ef696431b5dc414c0cb
Walter Moreira <walter@waltermoreira.net>
2015-01-24 15:02:36 -0600

    Inject parent info in children

--- serfnode/build/handler/launcher.py
+++ serfnode/build/handler/launcher.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import functools
+import json
 import os
 import signal
 import sys
 
 import docker_utils
+from info import NODE_INFO
 
 
 def handler(name, signum, frame):
@@ -32,7 +34,20 @@ def launch(name, args):
     except Exception:
         pass
     args.insert(0, '--cidfile=/child_{}'.format(name))
-    docker_utils.docker('run', *args)
+    docker_utils.docker('run', '-d', *args)
+
+
+def inject_ip(name):
+    cid = open('/child_{}'.format(name)).read().strip()
+    content = json.dumps(NODE_INFO)
+    docker_utils.docker('exec', cid,
+                        'bash', '-c',
+                        "echo '{}' > /parent_info".format(content))
+
+
+def wait(name):
+    cid = open('/child_{}'.format(name)).read().strip()
+    docker_utils.docker('wait', cid)
 
 
 if __name__ == '__main__':
@@ -40,3 +55,5 @@ if __name__ == '__main__':
     args = sys.argv[2:]
     signal.signal(signal.SIGINT, functools.partial(handler, name))
     launch(name, args)
+    inject_ip(name)
+    wait(name)

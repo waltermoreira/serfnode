16c6f4f7d6120d8be03dff5063febf9bb44207c8
Walter Moreira <walter@waltermoreira.net>
2015-01-30 17:03:15 -0600

    More experiments with self deployment

--- serfnode/deploy/deploy.py
+++ serfnode/deploy/deploy.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import argparse
+import Queue
 import subprocess
 import sys
+import threading
+
+import yaml
 
 
 def render_host(ip, user, key=None):
@@ -14,11 +20,55 @@ def render_host(ip, user, key=None):
             '{} ansible_ssh_user={} {}\n'.format(ip, user, key_line))
 
 
+def progress():
+    queue = Queue.Queue()
+    def _progress(queue):
+        print('\n')
+        while True:
+            try:
+                queue.get(timeout=1)
+                print('\n')
+                return
+            except Queue.Empty:
+                print('.', sep='', end='')
+                sys.stdout.flush()
+    t = threading.Thread(target=_progress, args=(queue,))
+    t.daemon = True
+    t.start()
+    return queue
+
+
 def ansible(args):
     ask = '--ask-pass' if not args.key else ''
-    cmd = ('ansible-playbook {} -i /deploy/hosts /deploy/deploy.yml'
-           .format(ask))
-    subprocess.check_call(cmd.split())
+    if args.deploy:
+        playbook = 'deploy'
+    elif args.stop:
+        playbook = 'stop'
+    elif args.docker:
+        playbook = 'docker'
+    else:
+        print('need one of --deploy, --stop, --docker')
+        return
+    cmd = ('ansible-playbook {} -i /deploy/hosts /deploy/{}.yml'
+           .format(ask, playbook))
+    if playbook == 'stop':
+        serfnode_name = subprocess.check_output(
+            'cat /deploy/deploy.yml | grep serfnode_name | '
+            'cut -f 2 -d: | head -1',
+            shell=True).strip()
+        cmd += ' -e serfnode_name={}'.format(serfnode_name)
+    #p = progress()
+    ansible_process = subprocess.Popen(cmd.split(),
+                                       stderr=subprocess.PIPE,
+                                       stdout=subprocess.PIPE)
+    err = ansible_process.wait()
+    #p.put(None)
+    if err:
+        print('Ansible playbook failed:')
+        print(ansible_process.stderr.read())
+        print(ansible_process.stdout.read())
+        return
+    print(open('/tmp/me').read())
 
 
 def main():
@@ -33,6 +83,8 @@ def parse_args(args):
                         help='self-deploy to a host')
     parser.add_argument('--stop', action='store_true',
                         help='stop itself in a host')
+    parser.add_argument('--docker', action='store_true',
+                        help='install docker in remote host')
     parser.add_argument('--ip', nargs='?', metavar='IP',
                         type=str, default='172.17.42.1',
                         help='ip of host to deploy')

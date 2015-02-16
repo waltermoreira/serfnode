eced837f9dfc247700d2325e11e169e78bd82ba3
Walter Moreira <walter@waltermoreira.net>
2015-02-16 16:29:58 -0600

    Use serfnode.yml as source of defaults for config

--- serfnode/handler/config.py
+++ serfnode/handler/config.py
@@ -2,13 +2,24 @@ import os
 import uuid
 
 from mischief.actors.pipe import get_local_ip
+import yaml
 
 
-role = os.environ.get('ROLE') or 'no_role'
-peer = os.environ.get('PEER')
-ip = os.environ.get('IP') or get_local_ip('8.8.8.8')
-bind_port = os.environ.get('SERF_PORT') or 7946
+def read_serfnode_yml():
+    with open('/serfnode.yml') as input:
+        conf = yaml.load(input) or {}
+        return conf['serfnode']
+
+
+yml = read_serfnode_yml()
+
+
+role = os.environ.get('ROLE') or yml.get('ROLE') or 'no_role'
+peer = os.environ.get('PEER') or yml.get('PEER')
+ip = (os.environ.get('SERF_IP') or yml.get('SERF_IP') or
+      get_local_ip('8.8.8.8'))
+bind_port = os.environ.get('SERF_PORT') or yml.get('SERF_PORT') or 7946
 node = os.environ.get('NODE_NAME') or uuid.uuid4().hex
 rpc_port = os.environ.get('RPC_PORT') or 7373
-service = os.environ.get('SERVICE_IP') or ip
-service_port = os.environ.get('SERVICE_PORT') or 0
+service = os.environ.get('SERVICE_IP') or yml.get('SERVICE_IP') or ip
+service_port = os.environ.get('SERVICE_PORT') or yml.get('SERVICE_PORT') or 0

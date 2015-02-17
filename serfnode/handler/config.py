3139ae7dceb3605e70db2cbcde0d732dcb68bc2a
Walter Moreira <walter@waltermoreira.net>
2015-02-17 16:23:08 -0600

    Make yaml fields lowercase in serfnode section

--- serfnode/handler/config.py
+++ serfnode/handler/config.py
@@ -14,12 +14,12 @@ def read_serfnode_yml():
 yml = read_serfnode_yml()
 
 
-role = os.environ.get('ROLE') or yml.get('ROLE') or 'no_role'
-peer = os.environ.get('PEER') or yml.get('PEER')
-ip = (os.environ.get('SERF_IP') or yml.get('SERF_IP') or
+role = os.environ.get('ROLE') or yml.get('role') or 'no_role'
+peer = os.environ.get('PEER') or yml.get('peer')
+ip = (os.environ.get('SERF_IP') or yml.get('serf_ip') or
       get_local_ip('8.8.8.8'))
-bind_port = os.environ.get('SERF_PORT') or yml.get('SERF_PORT') or 7946
+bind_port = os.environ.get('SERF_PORT') or yml.get('serf_port') or 7946
 node = os.environ.get('NODE_NAME') or uuid.uuid4().hex
 rpc_port = os.environ.get('RPC_PORT') or 7373
-service = os.environ.get('SERVICE_IP') or yml.get('SERVICE_IP')
-service_port = os.environ.get('SERVICE_PORT') or yml.get('SERVICE_PORT') or 0
+service = os.environ.get('SERVICE_IP') or yml.get('service_ip')
+service_port = os.environ.get('SERVICE_PORT') or yml.get('service_port') or 0

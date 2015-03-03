4284cff2465bff8ec4f42bdd415876e9efdf8cd5
Walter Moreira <walter@waltermoreira.net>
2015-03-03 14:49:05 -0600

    Add parent's net info to children info

--- serfnode/handler/children_server.py
+++ serfnode/handler/children_server.py
@@ -3,9 +3,11 @@
 import json
 import time
 import os
+import socket
 
 import pipe
 from join import get_info
+import docker_utils
 
 
 def handler(obj):
@@ -16,6 +18,8 @@ def handler(obj):
     while not os.path.exists('/agent_up'):
         time.sleep(0.1)
     new = get_info(obj)
+    parent = docker_utils.client.inspect_container(socket.gethostname())
+    new['inspect']['NetworkSettings'] = parent['NetworkSettings']
     name = new['inspect']['Name'][1:]
     old[name] = new
     with open('/children_by_name.json', 'w') as f:
